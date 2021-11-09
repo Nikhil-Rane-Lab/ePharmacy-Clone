320e510f187684b7acd040e28f7dee200d0cc599
Suraj Verma <vermasuraj0008@gmail.com>
2021-11-09 18:30:57 +0530

    adding seperate js file for home

---
+++ frontend/home/home.js
@@ -0,0 +1,6 @@
+import { structureTop, structureBottom } from '../component/outerStructure/structure.js';
+
+let topStructure = document.querySelector('.structureTop');
+let bottomStructure = document.querySelector('.structureBottom');
+topStructure.innerHTML = structureTop();
+bottomStructure.innerHTML = structureBottom();
