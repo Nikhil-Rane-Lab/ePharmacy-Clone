2409ebc0a30e346fb0bb9347d743ff94672bf500
Suraj Verma <vermasuraj0008@gmail.com>
2021-11-12 15:44:01 +0530

    adding some changes in the diagnostic.js file

--- frontend/diagnostic/diagnostic.js
+++ frontend/diagnostic/diagnostic.js
@@ -11,20 +11,25 @@ home.addEventListener('click', () => {
 });
 
 let display = document.querySelectorAll('.right-popular');
-console.log('display:', display);
-
+let select_div = 0;
 display.forEach((e) => {
 	e.addEventListener('click', () => {
-		console.log(e.parentElement.firstElementChild);
+		select_div = e;
+		console.log(select_div);
+		for (let i = 0; i < 3; i++) {
+			let parent1 = display[i].parentElement.firstElementChild.children;
+			let price1 = parent1[2];
+			let content1 = parent1[3];
+			content1.classList.add('display-property-none');
+			price1.classList.remove('display-property-none');
+			display[i].classList.remove('display-property-none');
+		}
 		let parent = e.parentElement.firstElementChild.children;
-		console.log(parent);
-
-		console.log(parent.length);
 		let price = parent[2];
 		let content = parent[3];
 		content.classList.remove('display-property-none');
 		price.classList.add('display-property-none');
 		e.classList.add('display-property-none');
 	});
+	console.log('select_div:', select_div);
 });
-
