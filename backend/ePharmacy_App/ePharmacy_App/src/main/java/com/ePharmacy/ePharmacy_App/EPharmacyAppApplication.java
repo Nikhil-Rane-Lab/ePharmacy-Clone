c37747272c099de2fea6d03b07cd32850a974a08
Suraj Verma <vermasuraj0008@gmail.com>
2021-11-12 16:30:38 +0530

    adding the backend folder

---
+++ backend/ePharmacy_App/ePharmacy_App/src/main/java/com/ePharmacy/ePharmacy_App/EPharmacyAppApplication.java
@@ -0,0 +1,13 @@
+package com.ePharmacy.ePharmacy_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPharmacyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPharmacyAppApplication.class, args);
+	}
+
+}
---
+++ backend/ePharmacy_App/ePharmacy_App/src/test/java/com/ePharmacy/ePharmacy_App/EPharmacyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ePharmacy.ePharmacy_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPharmacyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
