bd5b566af9994bb69676c27ff6205679d73ada0a
Rahul Sheelavantar <rahulsheelavantar@gmail.com>
2021-11-13 13:53:32 +0530

    done Modal

--- frontend/home/home.js
+++ frontend/home/home.js
@@ -10,6 +10,22 @@ diagnostic.addEventListener('click', () => {
 	window.location.href = '../diagnostic/diagnostic.html';
 });
 
+let focus = document.querySelector('.deliver-to');
+let modal = document.querySelector('.modal');
+let closeModal = document.querySelector('#closeModal');
+console.log(closeModal);
+modal.style.display = "none";
+closeModal.addEventListener('click',()=>{
+	modal.style.display = 'none'
+});
+focus.addEventListener('click',()=>{
+	console.log("true");
+	modal.style.display = "block";
+});
+if(modal.style.display == "block"){
+	
+}
+
 // function timeClock(){
 //     setTimeout("timeClock()", 1000);        
 //     now = new Date();
