b7c785ac14a83e5e7cec184f5d17fe2b05c00b1d
Suraj Verma <vermasuraj0008@gmail.com>
2021-11-13 18:15:39 +0530

    adding frontend change

--- frontend/product-display/product-display.js
+++ frontend/product-display/product-display.js
@@ -2,3 +2,12 @@ import { structureTop} from '../component/outerStructure/structure.js';
 
 let topStructure = document.querySelector('.structureTop');
 topStructure.innerHTML = structureTop();
+
+async function showCharacter() {
+    let res = await fetch(`http://localhost:8080/product`)
+    let data = await res.json()
+    console.log('data:', data)
+}
+
+showCharacter()
+
