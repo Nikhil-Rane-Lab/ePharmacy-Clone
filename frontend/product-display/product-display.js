8c2804aac7096c88037b8c6d8b1072d9cfeaea53
Suraj Verma <vermasuraj0008@gmail.com>
2021-11-13 19:38:29 +0530

    adding the new changes in product display page

--- frontend/product-display/product-display.js
+++ frontend/product-display/product-display.js
@@ -3,10 +3,17 @@ import { structureTop} from '../component/outerStructure/structure.js';
 let topStructure = document.querySelector('.structureTop');
 topStructure.innerHTML = structureTop();
 
+let search = 
 async function showCharacter() {
     let res = await fetch(`http://localhost:8080/product`)
     let data = await res.json()
     console.log('data:', data)
+    array.forEach(element => {
+        if(element = search){
+            
+        }
+    });
+
 }
 
 showCharacter()
