8b1144d2a4eb2c0a30c2f75a1e22c2cdff896709
Suraj Verma <vermasuraj0008@gmail.com>
2021-11-14 13:02:47 +0530

    adding timer to the cart page

--- frontend/cart/cartmodule.js
+++ frontend/cart/cartmodule.js
@@ -2,3 +2,6 @@
 import { structureTop } from '../component/outerStructure/structure.js';
 
 document.getElementById('structureTop').innerHTML = structureTop();
+
+import timer from '../component/displayTime/time.js'
+timer();
