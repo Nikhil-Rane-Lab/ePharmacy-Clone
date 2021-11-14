8b07e50b58e93cea09750bb95a617f33aba606b7
Rahul <1996meenarahul@gmail.com>
2021-11-14 18:30:47 +0530

    backend

--- backend/ePharmacy_App/ePharmacy_App/src/main/java/com/ePharmacy/ePharmacy_App/controller/CartItemController.java
+++ backend/ePharmacy_App/ePharmacy_App/src/main/java/com/ePharmacy/ePharmacy_App/controller/CartItemController.java
@@ -1,13 +1,22 @@
 package com.ePharmacy.ePharmacy_App.controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CartItemController {
 
 
-//  @PostMapping("/addtocart/{userid}/{productId}/{")
+   @PostMapping("/addtocart/{userid}/{productid}/{quantity}")
+   public String addtoCart(@PathVariable("userid") Long userId , @PathVariable("productId") Long prodId, @PathVariable("quantity") int qty ){
+
+
+
+      return "product added";
+   }
 
 }
--- backend/ePharmacy_App/ePharmacy_App/src/main/java/com/ePharmacy/ePharmacy_App/service/CartItemService.java
+++ backend/ePharmacy_App/ePharmacy_App/src/main/java/com/ePharmacy/ePharmacy_App/service/CartItemService.java
@@ -1,9 +1,52 @@
 package com.ePharmacy.ePharmacy_App.service;
 
+import com.ePharmacy.ePharmacy_App.entity.Cart;
+import com.ePharmacy.ePharmacy_App.entity.Cart_Item;
+import com.ePharmacy.ePharmacy_App.entity.Product;
+import com.ePharmacy.ePharmacy_App.entity.User;
+import com.ePharmacy.ePharmacy_App.repository.CartItemRepository;
+import com.ePharmacy.ePharmacy_App.repository.ProductRepository;
+import com.ePharmacy.ePharmacy_App.repository.UserRepository;
+import org.apache.catalina.LifecycleState;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CartItemService {
 
+    @Autowired
+    CartItemRepository cartItemRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    public String addtoCart(Long userId, Long prodId , int qty){
+
+        User user = userRepository.findById(userId).get();
+
+        Product product = productRepository.findById(prodId).get();
+
+        Cart cart = user.getCart();
+
+        List<Cart_Item> cartItems = cart.getCart_items() ;
+
+        //check If purchased product is already existing in the cartitem  table;
+        for(Cart_Item cartItem : cartItems){
+            if(cartItem.getProduct().getProduct_id() ==prodId){
+               cartItem.setQuantity(qty);
+               cartItem.setTotal(product.getPrice()*qty);
+               cartItemRepository.save(cartItem);
+            }
+        }
+
+//        Cart_Item cartItem
+
+        return "ok";
+    }
 
 }
