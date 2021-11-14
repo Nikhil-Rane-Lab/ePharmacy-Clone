8a9c9f2db420cf4db6a9d1d3631b5d82e74eadba
Nikhil Rane <70564188+Nikhil-Rane-Lab@users.noreply.github.com>
2021-11-14 19:39:33 +0530

    Add files via upload

---
+++ frontend/component/clock/clock.js
@@ -0,0 +1,26 @@
+function timerClock() 
+{
+	var currentTime = new Date();
+	var hours = currentTime.getHours();
+	var minutes = currentTime.getMinutes();
+	var sec = currentTime.getSeconds();
+	if (minutes < 10) {
+		minutes = '0' + minutes;
+	}
+	if (sec < 10) {
+		sec = '0' + sec;
+	}
+	var t_str = hours + ':' + minutes + ':' + sec + ' ';
+	if (hours > 11) {
+		t_str += 'PM';
+	} else {
+		t_str += 'AM';
+	}
+	let t_insert = document.querySelector('.left-top');
+	t_insert.innerHTML = t_str;	
+
+	setTimeout(timer,1000 );
+}
+
+setInterval(timerClock, 1000);
+
