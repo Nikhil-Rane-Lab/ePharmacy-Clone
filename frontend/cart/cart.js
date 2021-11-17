9e3cd707f86852f5f5cbe9b02bcb50419e308984
Rahul <1996meenarahul@gmail.com>
2021-11-17 17:42:43 +0530

    increase and Deacrese quantity in Cart Done

--- frontend/cart/cart.js
+++ frontend/cart/cart.js
@@ -1,10 +1,4 @@
 
-
-
-
-  
- showCartItems();
-
  
   var cart_total = 0;
   async function showCartItems(){
@@ -24,6 +18,9 @@
         cart_total = cart_total + e.total;
   
         let itemBoxDiv = document.createElement('div');
+    
+        itemBoxDiv.setAttribute("id",`${e.cartItem_id}`);
+        
 
         let prescrpt = "";
 
@@ -42,9 +39,9 @@
                  <div id="price"> ${e.product.price} Rs</div>
               </div>
               <div id="itemOperation" >
-                  <a href="#" id="${e.cartItem_id}" > <img id="minus" src="../icons/cart/minus.svg" alt=""> </a>
-                  <p class="quantity">1</p>
-                  <a href="#" id="${e.cartItem_id}">  <img id="plus" src="../icons/cart/plus.svg" alt=""> </a>
+                  <a  onclick="decreaseQty(this)"   id="${e.product.product_id}" > <img id="minus" src="../icons/cart/minus.svg" alt=""> </a>
+                  <p class="quantity" id="itemQuant${e.product.product_id}" >  ${e.quantity}   </p>
+                  <a  onclick="increaseQty(this)" id="${e.product.product_id}">  <img id="plus" src="../icons/cart/plus.svg" alt=""> </a>
                   <button onclick="deleteItem(this)" id="${e.cartItem_id}" class="deleteItem">Delete</button>
               </div> `;
               
@@ -56,6 +53,8 @@
      document.getElementById("totalAmount").innerHTML = `${finalAmount} Rs`;
 
   }
+
+  showCartItems();
 
   var cartTotal = 0;
   async function applyCoupon(){
@@ -107,13 +106,107 @@
 
 }
 
+var totalAfterUpdate =0;
+async function totalCartAmount(){
+    
+    let user = JSON.parse(localStorage.getItem("userDetail"));
+      
+    let res = await fetch(`http://localhost:8080/cartItems/user/${user.userId}`);
+    let data = await res.json();
+   
+    data.forEach((e)=>{
+        totalAfterUpdate = totalAfterUpdate + e.total;
+    });
+    
+    console.log(totalAfterUpdate);
+
+    document.getElementById("productsMRP").innerText = `${totalAfterUpdate} Rs`;
+    var cartfinalAmount = totalAfterUpdate + 80 ;
+    document.getElementById("totalAmount").innerText = `${cartfinalAmount} Rs`;
+}
+
 async function deleteItem(cartItemId){
 
     console.log(cartItemId.id);
 
-    let res = await fetch(`http://localhost:8080/delete/Item/${cartItemId.id}`,{method : 'DELETE'});
-    let data = await res.json();
-    location.reload();
+    let res = await fetch(`http://localhost:8080/delete/Item/${cartItemId.id}`,{
+        method :'DELETE',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'  
+        }
+    });
+     
+ 
+    window.alert("Item has been Deleted");
+    
+    window.location.reload();
+
+}
 
 
+var timerId;
+function debounce(func,delay,prodId,updatedQty){
+  
+    
+    if(timerId){
+        clearTimeout(timerId);
+    }
+
+   
+    timerId = setTimeout(()=> {
+        func(prodId,updatedQty);
+    },delay);
+  }
+ 
+   
+function decreaseQty(productID){
+
+    var temp = "itemQuant" + productID.id;
+    
+     
+     
+    console.log(productID.id)
+
+    var currentQty = Number(document.getElementById(temp).innerText);
+     
+   
+    if(currentQty>1){
+    document.getElementById(temp).innerText = currentQty - 1; 
+    var updatedQty = currentQty-1;
+    debounce(updateQty,500,productID,updatedQty);
+    }
+    
+}
+
+
+function increaseQty(productID){
+    var temp = "itemQuant" + productID.id;
+
+    
+    var currentQty = Number(document.getElementById(temp).innerText);
+      
+      document.getElementById(temp).innerText = currentQty + 1;
+
+      var updatedQty = currentQty + 1;
+
+        debounce(updateQty,500,productID,updatedQty);
+}
+
+async function updateQty(productID,updatedQty){
+
+
+    let userId = JSON.parse(localStorage.getItem('userDetail')).userId;
+			fetch(`http://localhost:8080/addtocart/users/${userId}/product/${productID.id}`, {
+				method: 'POST',
+				body: JSON.stringify({
+					quantity: updatedQty
+				}),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+
+             
+            setTimeout(totalCartAmount,3500);
+            
 }
